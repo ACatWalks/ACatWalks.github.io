6e11013cf6f099fea8315d5ded03ae7d5c60afb1
Amanda Woods <acwoods314@gmail.com>
2024-02-22 08:33:59 -0800

    Update App.js

--- ACatWalks.github.io/portfolio/src/App.js
+++ ACatWalks.github.io/portfolio/src/App.js
@@ -1,14 +1,11 @@
-import Nav from './views/Nav'
-import Footer from './views/Footer'
+
 import About from './views/About'
 import './styles/styles.scss';
 
 function App() {
   return (
     <div className="App">
-      <Nav />
       <About />
-      <Footer />
     </div>
   );
 }
